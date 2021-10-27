f3afad1e4dab5012af7cca3b6865179dbee05c80
eyakovlev <yakovlev.es@icloud.com>
2021-10-27 18:55:45 +0300

    Initial commit

---
+++ src/main/java/com/hexletlection/introapp/IntroAppApplication.java
@@ -0,0 +1,13 @@
+package com.hexletlection.introapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hexletlection/introapp/IntroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexletlection.introapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
